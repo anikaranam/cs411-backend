a2a78666aa24ef03c5e6921cf165bf9b8be1df84
ankitpatel99 <62448844+ankitpatel99@users.noreply.github.com>
2020-05-01 22:29:46 -0400

    backend endpoints addded

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,8 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "anrMar#18",
-  database: "cs411"
+  password: "ABC12345",
+  database: "CS411 Project"
 });
 
 /* GET home page. */
@@ -29,6 +29,259 @@ router.get('/playerData', function(req, res, next) {
 		res.send(output);
 	})
 });
+
+
+router.get('/playerPointsPerGame', function(req, res, next) {
+	var playerName = req.query.name;
+	console.log(playerName);
+
+	con.query("select * from Player where PlayerName = '" + playerName + "'", function(err,output){
+		if (err)
+			throw err;
+		if (output.length == 0)
+			res.send("No Player Exists");
+	})
+
+	con.query("select PointsPerGame from Player where PlayerName = '" + playerName + "'", function(err, output) {
+		if (err)
+			throw err;
+		res.send(output);
+	})
+});
+
+
+router.get('/playerAssistsPerGame', function(req, res, next) {
+	var playerName = req.query.name;
+	console.log(playerName);
+
+	con.query("select AssistsPerGame from Player where PlayerName = '" + playerName + "'", function(err, output) {
+		if (err)
+			throw err;
+		res.send(output);
+	})
+});
+
+
+router.get('/playerReboundsPerGame', function(req, res, next) {
+	var playerName = req.query.name;
+	console.log(playerName);
+
+	con.query("select ReboundsPerGame from Player where PlayerName = '" + playerName + "'", function(err, output) {
+		if (err)
+			throw err;
+		res.send(output);
+	})
+});
+
+
+router.get('/playerBlocksPerGame', function(req, res, next) {
+	var playerName = req.query.name;
+	console.log(playerName);
+
+	con.query("select BlocksPerGame from Player where PlayerName = '" + playerName + "'", function(err, output) {
+		if (err)
+			throw err;
+		res.send(output);
+	})
+});
+
+
+router.get('/playerStealsPerGame', function(req, res, next) {
+	var playerName = req.query.name;
+	console.log(playerName);
+
+	con.query("select StealsPerGame from Player where PlayerName = '" + playerName + "'", function(err, output) {
+		if (err)
+			throw err;
+		res.send(output);
+	})
+});
+
+
+router.get('/playerGamesPlayed', function(req, res, next) {
+	var playerName = req.query.name;
+	console.log(playerName);
+
+	con.query("select GamesPlayed from Player where PlayerName = '" + playerName + "'", function(err, output) {
+		if (err)
+			throw err;
+		res.send(output);
+	})
+});
+
+
+router.get('/playerHomeState', function(req, res, next) {
+	var playerName = req.query.name;
+	console.log(playerName);
+
+	con.query("select BirthState from Player where PlayerName = '" + playerName + "'", function(err, output) {
+		if (err)
+			throw err;
+		res.send(output);
+	})
+});
+
+
+router.get('/playerCollege', function(req, res, next) {
+	var playerName = req.query.name;
+	console.log(playerName);
+
+	con.query("select College from Player where PlayerName = '" + playerName + "'", function(err, output) {
+		if (err)
+			throw err;
+		res.send(output);
+	})
+});
+
+router.get('/coachWins', function(req, res, next) {
+	var coachName = req.query.name;
+	console.log(coachName);
+
+	con.query("select CareerWins from Coach where CoachName = '" + coachName + "'", function(err, output) {
+		if (err)
+			throw err;
+		res.send(output);
+	})
+});
+
+
+router.get('/coachLosses', function(req, res, next) {
+	var coachName = req.query.name;
+	console.log(coachName);
+
+	con.query("select CareerLosses from Coach where CoachName = '" + coachName + "'", function(err, output) {
+		if (err)
+			throw err;
+		res.send(output);
+	})
+});
+
+
+router.get('/coachSeasons', function(req, res, next) {
+	var coachName = req.query.name;
+	console.log(coachName);
+
+	con.query("select SeasonsActive from Coach where CoachName = '" + coachName + "'", function(err, output) {
+		if (err)
+			throw err;
+		res.send(output);
+	})
+});
+
+
+router.get('/coachTeamCoached', function(req, res, next) {
+	var coachName = req.query.name;
+	console.log(coachName);
+
+	con.query("select Manages from Coach where CoachName = '" + coachName + "'", function(err, output) {
+		if (err)
+			throw err;
+		res.send(output);
+	})
+});
+
+
+router.get('/coachPointsPerGame', function(req, res, next) {
+	var coachName = req.query.name;
+	console.log(coachName);
+
+	con.query("select T.PointsScored from Coach C left outer join Team T on C.Manages = T.TeamName where C.CoachName = '" + coachName + "'", function(err, output) {
+		if (err)
+			throw err;
+		res.send(output);
+	})
+});
+
+
+router.get('/teamWins', function(req, res, next) {
+	var teamName = req.query.name;
+	console.log(teamName);
+
+	con.query("select count(*) from Game where Winner = '" + teamName + "'", function(err, output) {
+		if (err)
+			throw err;
+		res.send(output);
+	})
+});
+
+
+router.get('/teamLosses', function(req, res, next) {
+	var teamName = req.query.name;
+	console.log(teamName);
+
+	con.query("select 82 - count(*) from Game where Winner = '" + teamName + "'", function(err, output) {
+		if (err)
+			throw err;
+		res.send(output);
+	})
+});
+
+
+router.get('/teamPoints', function(req, res, next) {
+	var teamName = req.query.name;
+	console.log(teamName);
+
+	con.query("select PointsScored from Team where TeamName = '" + teamName + "'", function(err, output) {
+		if (err)
+			throw err;
+		res.send(output);
+	})
+});
+
+
+router.get('/teamHomeWins', function(req, res, next) {
+	var teamName = req.query.name;
+	console.log(teamName);
+
+	con.query("select count(*) from Game where Winner = '" + teamName + "' and HomeTeam = '" + teamName + "'" , function(err, output) {
+		if (err)
+			throw err;
+		res.send(output);
+	})
+});
+
+
+router.get('/teamAwayWins', function(req, res, next) {
+	var teamName = req.query.name;
+	console.log(teamName);
+
+	con.query("select count(*) from Game where Winner = '" + teamName + "' and AwayTeam = '" + teamName + "'" , function(err, output) {
+		if (err)
+			throw err;
+		res.send(output);
+	})
+});
+
+// not sure if I did this correctly
+router.get('/teamPlayersFromState', function(req, res, next) {
+	var teamName = req.query.name;
+    console.log(teamName);
+    var birthState = req.query.state;
+    console.log(birthState);
+
+	con.query("select count(*) from Player where PlaysFor = '" + teamName + "' and BirthState = '" + birthState + "'" , function(err, output) {
+		if (err)
+			throw err;
+		res.send(output);
+	})
+});
+
+
+// not sure if I did this correctly
+router.get('/teamPlayersFromCollege', function(req, res, next) {
+	var teamName = req.query.name;
+    console.log(teamName);
+    var college = req.query.college;
+    console.log(college);
+
+	con.query("select count(*) from Player where PlaysFor = '" + teamName + "' and College = '" + college + "'" , function(err, output) {
+		if (err)
+			throw err;
+		res.send(output);
+	})
+});
+
+
 
 router.post('/addCoachRecord', function(req, res, next) {
 
