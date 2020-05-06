399b171353d55107a89a9385f790af1e51f64418
Anirudh Karanam <anirudhkaranam@Anirudhs-Macbook-Pro.local>
2020-05-06 01:06:10 -0500

    fixed bug

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,8 @@ const {spawn} = require('child_process');
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "nbavision",
-  database: "mydb"
+  password: "anrMar#18",
+  database: "cs411"
 });
 
 class player {
@@ -82,117 +82,131 @@ router.get('/playerData', function(req, res, next) {
 });
 
 
-router.get('/playerPointsPerGame', function(req, res, next) {
-	var playerName = req.query.name;
-	console.log(playerName);
+router.post('/playerPointsPerGame', function(req, res, next) {
+	
+	var players = req.body.toCompare;	
+	console.log(players);
 
-	con.query("select * from Player where PlayerName = '" + playerName + "'", function(err,output){
-		if (err)
-			throw err;
-		if (output.length == 0)
-			res.send("No Player Exists");
-	})
+	var ppgArr = [];
 
-	con.query("select PointsPerGame from Player where PlayerName = '" + playerName + "'", function(err, output) {
-		if (err)
-			throw err;
-		res.send(output);
-	})
+	for (let i = 0; i < players.length; i++) {
+		con.query("select PointsPerGame as ppg from Player where PlayerName = '" + players[i] + "'", function(err, output) {
+			if (err)
+				throw err;
+			ppgArr.push(output[0].ppg);
+			console.log(output[0].ppg);
+			console.log(ppgArr);
+			if (i == players.length - 1) {
+				res.send(ppgArr);
+			}
+		})
+	}
+	
 });
 
 
-router.get('/playerAssistsPerGame', function(req, res, next) {
-	var playerName = req.query.name;
-	console.log(playerName);
+router.post('/playerAssistsPerGame', function(req, res, next) {
+	var players = req.body.toCompare;	
+	console.log(players);
 
-	con.query("select * from Player where PlayerName = '" + playerName + "'", function(err,output){
-		if (err)
-			throw err;
-		if (output.length == 0)
-			res.send("No Player Exists");
-	})
+	var apgArr = [];
 
-	con.query("select AssistsPerGame from Player where PlayerName = '" + playerName + "'", function(err, output) {
-		if (err)
-			throw err;
-		res.send(output);
-	})
+	for (let i = 0; i < players.length; i++) {
+		con.query("select AssistsPerGame as apg from Player where PlayerName = '" + players[i] + "'", function(err, output) {
+			if (err)
+				throw err;
+			apgArr.push(output[0].apg);
+			console.log(output[0].apg);
+			console.log(apgArr);
+			if (i == players.length - 1) {
+				res.send(apgArr);
+			}
+		})
+	}
 });
 
 
-router.get('/playerReboundsPerGame', function(req, res, next) {
-	var playerName = req.query.name;
-	console.log(playerName);
+router.post('/playerReboundsPerGame', function(req, res, next) {
+	var players = req.body.toCompare;	
+	console.log(players);
 
-	con.query("select * from Player where PlayerName = '" + playerName + "'", function(err,output){
-		if (err)
-			throw err;
-		if (output.length == 0)
-			res.send("No Player Exists");
-	})
+	var rpgArr = [];
 
-	con.query("select ReboundsPerGame from Player where PlayerName = '" + playerName + "'", function(err, output) {
-		if (err)
-			throw err;
-		res.send(output);
-	})
+	for (let i = 0; i < players.length; i++) {
+		con.query("select ReboundsPerGame as rpg from Player where PlayerName = '" + players[i] + "'", function(err, output) {
+			if (err)
+				throw err;
+			rpgArr.push(output[0].rpg);
+			console.log(output[0].rpg);
+			console.log(rpgArr);
+			if (i == players.length - 1) {
+				res.send(rpgArr);
+			}
+		})
+	}
 });
 
 
-router.get('/playerBlocksPerGame', function(req, res, next) {
-	var playerName = req.query.name;
-	console.log(playerName);
+router.post('/playerBlocksPerGame', function(req, res, next) {
+	var players = req.body.toCompare;	
+	console.log(players);
 
-	con.query("select * from Player where PlayerName = '" + playerName + "'", function(err,output){
-		if (err)
-			throw err;
-		if (output.length == 0)
-			res.send("No Player Exists");
-	})
+	var bpgArr = [];
 
-	con.query("select BlocksPerGame from Player where PlayerName = '" + playerName + "'", function(err, output) {
-		if (err)
-			throw err;
-		res.send(output);
-	})
+	for (let i = 0; i < players.length; i++) {
+		con.query("select BlocksPerGame as bpg from Player where PlayerName = '" + players[i] + "'", function(err, output) {
+			if (err)
+				throw err;
+			bpgArr.push(output[0].bpg);
+			console.log(output[0].bpg);
+			console.log(bpgArr);
+			if (i == players.length - 1) {
+				res.send(bpgArr);
+			}
+		})
+	}
 });
 
 
-router.get('/playerStealsPerGame', function(req, res, next) {
-	var playerName = req.query.name;
-	console.log(playerName);
+router.post('/playerStealsPerGame', function(req, res, next) {
+	var players = req.body.toCompare;	
+	console.log(players);
 
-	con.query("select * from Player where PlayerName = '" + playerName + "'", function(err,output){
-		if (err)
-			throw err;
-		if (output.length == 0)
-			res.send("No Player Exists");
-	})
+	var spgArr = [];
 
-	con.query("select StealsPerGame from Player where PlayerName = '" + playerName + "'", function(err, output) {
-		if (err)
-			throw err;
-		res.send(output);
-	})
+	for (let i = 0; i < players.length; i++) {
+		con.query("select StealsPerGame as spg from Player where PlayerName = '" + players[i] + "'", function(err, output) {
+			if (err)
+				throw err;
+			spgArr.push(output[0].spg);
+			console.log(output[0].spg);
+			console.log(spgArr);
+			if (i == players.length - 1) {
+				res.send(spgArr);
+			}
+		})
+	}
 });
 
 
-router.get('/playerGamesPlayed', function(req, res, next) {
-	var playerName = req.query.name;
-	console.log(playerName);
+router.post('/playerGamesPlayed', function(req, res, next) {
+	var players = req.body.toCompare;	
+	console.log(players);
 
-	con.query("select * from Player where PlayerName = '" + playerName + "'", function(err,output){
-		if (err)
-			throw err;
-		if (output.length == 0)
-			res.send("No Player Exists");
-	})
+	var gpArr = [];
 
-	con.query("select GamesPlayed from Player where PlayerName = '" + playerName + "'", function(err, output) {
-		if (err)
-			throw err;
-		res.send(output);
-	})
+	for (let i = 0; i < players.length; i++) {
+		con.query("select GamesPlayed as gp from Player where PlayerName = '" + players[i] + "'", function(err, output) {
+			if (err)
+				throw err;
+			gpArr.push(output[0].gp);
+			console.log(output[0].gp);
+			console.log(gpArr);
+			if (i == players.length - 1) {
+				res.send(gpArr);
+			}
+		})
+	}
 });
 
 
@@ -347,18 +361,10 @@ router.get('/coachPointsPerGame', function(req, res, next) {
 });*/
 
 router.post('/teamWins', function(req, res, next) {
-	/*var teamName = req.query.name;
-	console.log(teamName);*/
 
 	var teams = req.body.toCompare;
 	console.log(teams);
 
-	/*con.query("select * from Team where TeamName = '" + teamName + "'", function(err,output){
-		if (err)
-			throw err;
-		if (output.length == 0)
-			res.send("No Team Exists");
-	})*/
 	var winsArr = [];
 	for (let i = 0; i < teams.length; i++) {
 
@@ -375,95 +381,98 @@ router.post('/teamWins', function(req, res, next) {
 			}
 		});
 	}
-	//console.log(winsArr + "   heeeere ");
-	//res.send(winsArr);
 
-	/*con.query("select count(*) as wins from Game where Winner = '" + teamName + "'", function(err, output) {
-		if (err)
-			throw err;
-		res.send(output);
-	})*/
 });
 
 
-router.get('/teamLosses', function(req, res, next) {
-	var teamName = req.query.name;
-	console.log(teamName);
+router.post('/teamLosses', function(req, res, next) {
 
-	con.query("select * from Team where TeamName = '" + teamhName + "'", function(err,output){
-		if (err)
-			throw err;
-		if (output.length == 0)
-			res.send("No Team Exists");
-	})
+	var teams = req.body.toCompare;
+	console.log(teams);
+	
 
+	var lossesArr = [];
+	for (let i = 0; i < teams.length; i++) {
+		con.query("select 82 - count(*) as losses from Game where Winner = '" + teams[i] + "'", function(err, output) {
+			if (err)
+				throw err;
+			lossesArr.push(output[0].losses);
+			console.log(output[0].losses);
+			console.log(lossesArr);
+			if (i == teams.length - 1) {
+				res.send(lossesArr);
+			}
+		})
+	}
 
-	con.query("select 82 - count(*) from Game where Winner = '" + teamName + "'", function(err, output) {
-		if (err)
-			throw err;
-		res.send(output);
-	})
+	
 });
 
 
-router.get('/teamPoints', function(req, res, next) {
-	var teamName = req.query.name;
-	console.log(teamName);
+router.post('/teamPoints', function(req, res, next) {
+	var teams = req.body.toCompare;
+	console.log(teams);
 
-	con.query("select * from Team where TeamName = '" + teamName + "'", function(err,output){
-		if (err)
-			throw err;
-		if (output.length == 0)
-			res.send("No Team Exists");
-	})
-
-	con.query("select PointsScored from Team where TeamName = '" + teamName + "'", function(err, output) {
-		if (err)
-			throw err;
-		res.send(output);
-	})
+	var pointsArr = [];
+	for (let i = 0; i < teams.length; i++) {
+		con.query("select PointsScored as points from Team where TeamName = '" + teams[i] + "'", function(err, output) {
+			if (err)
+				throw err;
+			pointsArr.push(output[0].points);
+			console.log(output[0].points);
+			console.log(pointsArr);
+			if (i == teams.length - 1) {
+				res.send(pointsArr);
+			}
+		})
+	}
 });
 
 
-router.get('/teamHomeWins', function(req, res, next) {
-	var teamName = req.query.name;
-	console.log(teamName);
+router.post('/teamHomeWins', function(req, res, next) {
 
-	con.query("select * from Team where TeamName = '" + teamhName + "'", function(err,output){
-		if (err)
-			throw err;
-		if (output.length == 0)
-			res.send("No Team Exists");
-	})
+	var teams = req.body.toCompare;
+	console.log(teams);
 
-	con.query("select count(*) from Game where Winner = '" + teamName + "' and HomeTeam = '" + teamName + "'" , function(err, output) {
-		if (err)
-			throw err;
-		res.send(output);
-	})
+	var homeWinsArr = [];
+
+	for (let i = 0; i < teams.length; i++) {
+		con.query("select count(*) as homeWins from Game where Winner = '" + teams[i] + "' and HomeTeam = '" + teams[i] + "'" , function(err, output) {
+			if (err)
+				throw err;
+				homeWinsArr.push(output[0].homeWins);
+				console.log(output[0].homeWins);
+				console.log(homeWinsArr);
+				if (i == teams.length - 1) {
+					res.send(homeWinsArr);
+				}
+		})
+	}
 });
 
 
-router.get('/teamAwayWins', function(req, res, next) {
-	var teamName = req.query.name;
-	console.log(teamName);
+router.post('/teamAwayWins', function(req, res, next) {
+	var teams = req.body.toCompare;
+	console.log(teams);
 
-	con.query("select * from Team where TeamName = '" + teamhName + "'", function(err,output){
-		if (err)
-			throw err;
-		if (output.length == 0)
-			res.send("No Team Exists");
-	})
+	var awayWinsArr = [];
 
-	con.query("select count(*) from Game where Winner = '" + teamName + "' and AwayTeam = '" + teamName + "'" , function(err, output) {
-		if (err)
-			throw err;
-		res.send(output);
-	})
+	for (let i = 0; i < teams.length; i++) {
+		con.query("select count(*) as awayWins from Game where Winner = '" + teams[i] + "' and AwayTeam = '" + teams[i] + "'" , function(err, output) {
+			if (err)
+				throw err;
+				awayWinsArr.push(output[0].awayWins);
+				console.log(output[0].awayWins);
+				console.log(awayWinsArr);
+				if (i == teams.length - 1) {
+					res.send(awayWinsArr);
+				}
+		})
+	}
 });
 
 // not sure if I did this correctly
-router.get('/teamPlayersFromState', function(req, res, next) {
+/*router.get('/teamPlayersFromState', function(req, res, next) {
 	var teamName = req.query.name;
     console.log(teamName);
     var birthState = req.query.state;
@@ -506,7 +515,7 @@ router.get('/teamPlayersFromCollege', function(req, res, next) {
 		res.send(output);
 	})
 });
-
+*/
 
 
 router.get('/teams', function(req, res, next) {
